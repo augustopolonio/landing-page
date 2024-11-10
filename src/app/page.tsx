97f4ba857a29be6171c188cfead2362ebf3f32f9
Augusto Polonio <augustodrp@gmail.com>
2024-11-09 21:54:28 -0300

    chore(shadcn): installed the button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,9 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
-	return <div>Page</div>;
+	return (
+		<div>
+			<Button>Click me</Button>
+		</div>
+	);
 }
